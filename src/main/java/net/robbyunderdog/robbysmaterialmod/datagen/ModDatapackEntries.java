68e7c867740225f6c4237cfa1a1c2f6ce7ffc1e8
Robert Warner <rw51380@gmail.com>
2025-04-08 23:16:03 -0700

    added world generation classes, ModBiomeModifiers, ModConfiguredFeatures, ModPlacedFeatures

--- src/main/java/net/robbyunderdog/robbysmaterialmod/datagen/ModDatapackEntries.java
+++ src/main/java/net/robbyunderdog/robbysmaterialmod/datagen/ModDatapackEntries.java
@@ -5,10 +5,14 @@ import net.minecraft.core.RegistrySetBuilder;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.PackOutput;
 import net.minecraftforge.common.data.DatapackBuiltinEntriesProvider;
+import net.minecraftforge.registries.ForgeRegistries;
 import net.robbyunderdog.robbysmaterialmod.RobbysMaterialMod;
 import net.robbyunderdog.robbysmaterialmod.enchantment.ModEnchantments;
 import net.robbyunderdog.robbysmaterialmod.trim.ModTrimMaterials;
 import net.robbyunderdog.robbysmaterialmod.trim.ModTrimPatterns;
+import net.robbyunderdog.robbysmaterialmod.worldgen.ModBiomeModifiers;
+import net.robbyunderdog.robbysmaterialmod.worldgen.ModConfiguredFeatures;
+import net.robbyunderdog.robbysmaterialmod.worldgen.ModPlacedFeatures;
 
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
@@ -18,7 +22,11 @@ public class ModDatapackEntries extends DatapackBuiltinEntriesProvider {
             .add(Registries.TRIM_MATERIAL, ModTrimMaterials::bootstrap)
             .add(Registries.TRIM_PATTERN, ModTrimPatterns::bootstrap)
 
-            .add(Registries.ENCHANTMENT, ModEnchantments::bootstrap);
+            .add(Registries.ENCHANTMENT, ModEnchantments::bootstrap)
+
+            .add(Registries.CONFIGURED_FEATURE, ModConfiguredFeatures::bootstrap)
+            .add(Registries.PLACED_FEATURE, ModPlacedFeatures::bootstrap)
+            .add(ForgeRegistries.Keys.BIOME_MODIFIERS, ModBiomeModifiers::bootstrap);
 
     public ModDatapackEntries(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
         super(output, registries, BUILDER, Set.of(RobbysMaterialMod.MOD_ID));
