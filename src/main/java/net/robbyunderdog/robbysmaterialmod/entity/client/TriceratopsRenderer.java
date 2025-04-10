88a3f81a3652638c5f92c2f75fc7593d7563d579
Robert Warner <rw51380@gmail.com>
2025-04-10 15:57:06 -0700

    added variants enum for triceratops + green triceratops variant

--- src/main/java/net/robbyunderdog/robbysmaterialmod/entity/client/TriceratopsRenderer.java
+++ src/main/java/net/robbyunderdog/robbysmaterialmod/entity/client/TriceratopsRenderer.java
@@ -1,21 +1,32 @@
 package net.robbyunderdog.robbysmaterialmod.entity.client;
 
+import com.google.common.collect.Maps;
 import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.Util;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.MobRenderer;
 import net.minecraft.resources.ResourceLocation;
 import net.robbyunderdog.robbysmaterialmod.RobbysMaterialMod;
+import net.robbyunderdog.robbysmaterialmod.entity.TriceratopsVariant;
 import net.robbyunderdog.robbysmaterialmod.entity.custom.TriceratopsEntity;
 
+import java.util.Map;
+
 public class TriceratopsRenderer extends MobRenderer<TriceratopsEntity, TriceratopsModel<TriceratopsEntity>> {
+    private static final Map<TriceratopsVariant, ResourceLocation> LOCATION_BY_VARIANT = Util.make(Maps.newEnumMap(TriceratopsVariant.class), map -> {
+        map.put(TriceratopsVariant.GRAY, ResourceLocation.fromNamespaceAndPath(RobbysMaterialMod.MOD_ID, "textures/entity/triceratops/triceratops_gray.png"));
+        map.put(TriceratopsVariant.GREEN, ResourceLocation.fromNamespaceAndPath(RobbysMaterialMod.MOD_ID, "textures/entity/triceratops/triceratops_green.png"));
+    });
+
+
     public TriceratopsRenderer(EntityRendererProvider.Context pContext) {
         super(pContext, new TriceratopsModel<>(pContext.bakeLayer(TriceratopsModel.LAYER_LOCATION)), 0.85f);
     }
 
     @Override
     public ResourceLocation getTextureLocation(TriceratopsEntity pEntity) {
-        return ResourceLocation.fromNamespaceAndPath(RobbysMaterialMod.MOD_ID, "textures/entity/triceratops/triceratops_gray.png");
+        return LOCATION_BY_VARIANT.get(pEntity.getVariant());
     }
 
     @Override
